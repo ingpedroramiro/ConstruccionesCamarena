aa56b42b2f3719f57e17573fe87c9f60bb9b596e
ingpedroramiro <yopedrorgz@gmail.com>
2024-09-05 17:28:03 -0700

    Avance 05/sep/24

---
+++ navegacion.php
@@ -0,0 +1,10 @@
+
+<link rel="stylesheet" href="styles.css">
+<h1 style="text-align: center; font-size: 30px;">Construcciones Camarena</h1>
+<nav>
+    <ul>
+        <li><a class="" role="button" href="pg_home">Inicio</a></li>
+        <li><a class="" role="button" href="pg_servicios" _black>Servicios</a></li>
+        <li><a class="" role="button" href="pg_contacto">Contacto</a></li>
+    </ul>
+</nav>
---
+++ pg_contacto.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Página Responsive</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+
+<body>
+    <header>
+        <?php include 'navegacion.php'; ?>
+    </header>
+    <main>
+        <section class="contenido">
+            <?php include 'contenido_contacto.php'; ?>
+        </section>
+        <aside class="sidebar">
+            Contacto
+            <?php include 'sidebar.php'; ?>
+        </aside>
+    </main>
+    <footer>
+        <?php include 'footer.php'; ?>
+    </footer>
+</body>
+
+</html>
